3980cd7a963c5aede423c8b24e6f96c976f7c062
Nazrin Burziyeva <nazrinburziyeva@gmail.com>
2025-06-26 16:33:58 +0400

    lesson04 solution.

--- src/test/kotlin/com/motycka/edu/lesson04/TemperatureConverterTest.kt
+++ src/test/kotlin/com/motycka/edu/lesson04/TemperatureConverterTest.kt
@@ -6,6 +6,17 @@ import io.kotest.matchers.shouldBe
 // By convention, the name of the test class should be the name of the class under test + "Test"
 class TemperatureConverterTest : StringSpec({
 
-    // TODO Implement tests for the TemperatureConverter class
+    "toCelsius should convert Fahrenheit to Celsius correctly" {
+        TemperatureConverter.toCelsius(32.0) shouldBe 0.0
+        TemperatureConverter.toCelsius(212.0) shouldBe 100.0
+        TemperatureConverter.toCelsius(98.6) shouldBe 37.0
+    }
+
+    "toFahrenheit should convert Celsius to Fahrenheit correctly" {
+        TemperatureConverter.toFahrenheit(0.0) shouldBe 32.0
+        TemperatureConverter.toFahrenheit(100.0) shouldBe 212.0
+        TemperatureConverter.toFahrenheit(37.0) shouldBe 98.6
+        TemperatureConverter.toFahrenheit(-10.0) shouldBe 14.0
+    }
 
 })
